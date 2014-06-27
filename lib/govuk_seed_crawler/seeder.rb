e6f3eb718cfb8cfb04d9b9358a27f9fe8ade4653
Matt Bostock <matt@mattbostock.com>
2014-06-27 11:39:04 +0100

    GetUrls#urls needs a site_root argument

--- lib/govuk_seed_crawler/seeder.rb
+++ lib/govuk_seed_crawler/seeder.rb
@@ -3,7 +3,7 @@ require 'topic_exchange'
 module GovukSeedCrawler
   class Seeder
     def self.seed(options = {})
-      urls = GetUrls.new.urls
+      urls = GetUrls.new.urls(options[:site_root])
       topic_exchange = TopicExchange.new(options)
 
       PublishUrls::publish(topic_exchange, options[:amqp_topic], urls)
