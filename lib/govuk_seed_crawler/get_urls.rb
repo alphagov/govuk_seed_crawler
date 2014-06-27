e32182e617ccc443f6cbe9a8440e0cb06cc1e258
Matt Bostock <matt@mattbostock.com>
2014-06-27 10:25:07 +0100

    Add GetUrls class

---
+++ lib/govuk_seed_crawler/get_urls.rb
@@ -0,0 +1,14 @@
+require 'govuk_mirrrorer/indexer'
+
+module GovukSeedCrawler
+  class GetUrls
+    attr_reader @urls
+
+    def initialize(site_root)
+      unless site_root raise "No site_root defined"
+
+      indexer = GovukMirrorer::Indexer.new(site_root)
+      @urls = indexer.all_start_urls
+    end
+  end
+end
