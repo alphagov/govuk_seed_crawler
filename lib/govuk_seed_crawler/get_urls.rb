88120e8f307ac0817ecdc71ff88df7fab99dfd94
Matt Bostock <matt@mattbostock.com>
2014-06-27 15:46:42 +0100

    Fix typo in error message

--- lib/govuk_seed_crawler/get_urls.rb
+++ lib/govuk_seed_crawler/get_urls.rb
@@ -5,7 +5,7 @@ module GovukSeedCrawler
     attr_reader :urls
 
     def initialize(site_root)
-      raise "No :site_root defined" unless site_root
+      raise "No site_root defined" unless site_root
 
       indexer = GovukMirrorer::Indexer.new(site_root)
       @urls = indexer.all_start_urls
