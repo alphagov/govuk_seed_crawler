e4c3d830f6f25af71eed337e4aca30cf248ee973
Matt Bostock <matt@mattbostock.com>
2014-06-27 11:38:33 +0100

    Make site_root a symbol
    It's lighter-weight compared to a string.

--- lib/govuk_seed_crawler/get_urls.rb
+++ lib/govuk_seed_crawler/get_urls.rb
@@ -4,10 +4,10 @@ module GovukSeedCrawler
   class GetUrls
     attr_reader @urls
 
-    def initialize(site_root)
-      unless site_root raise "No site_root defined"
+    def initialize(:site_root)
+      unless :site_root raise "No :site_root defined"
 
-      indexer = GovukMirrorer::Indexer.new(site_root)
+      indexer = GovukMirrorer::Indexer.new(:site_root)
       @urls = indexer.all_start_urls
     end
   end
