b2b9c7dc454495c5c11dd9bd99279f5bd806fec0
Matt Bostock <matt@mattbostock.com>
2014-06-27 11:35:50 +0100

    Remove unnecessary exception
    `PublishUrls::publish` should raise an error if we pass it an empty
    array of URLs.

--- lib/govuk_seed_crawler/seeder.rb
+++ lib/govuk_seed_crawler/seeder.rb
@@ -4,7 +4,6 @@ module GovukSeedCrawler
   class Seeder
     def self.seed(options = {})
       urls = GetUrls.new.urls
-      raise "No URLs were found" if urls.empty?
 
       topic_exchange = TopicExchange.new(options)
       PublishUrls::publish(topic_exchange, options[:amqp_topic], urls)
