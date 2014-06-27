d4293b6f52fcd2d39ec63130e5d1facab01966dd
Matt Bostock <matt@mattbostock.com>
2014-06-27 11:36:43 +0100

    Improve readability using vertical spacing

--- lib/govuk_seed_crawler/seeder.rb
+++ lib/govuk_seed_crawler/seeder.rb
@@ -4,8 +4,8 @@ module GovukSeedCrawler
   class Seeder
     def self.seed(options = {})
       urls = GetUrls.new.urls
-
       topic_exchange = TopicExchange.new(options)
+
       PublishUrls::publish(topic_exchange, options[:amqp_topic], urls)
     end
   end
