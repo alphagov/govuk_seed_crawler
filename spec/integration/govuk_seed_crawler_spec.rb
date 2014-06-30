c284b12ff00bd05575ea644708e3e80eb9f047c1
Matt Bostock <matt@mattbostock.com>
2014-06-30 13:48:15 +0100

    Add integration test

---
+++ spec/integration/govuk_seed_crawler_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'rabbitmq/http/client'
+
+describe GovukSeedCrawler do
+  options = {
+      :amqp_exchange => "govuk_seed_crawler_integration",
+      :amqp_topic => "#",
+      :amqp_vhost => "/",
+      :amqp_exchange_type => "topic",
+      :site_root => "https://www.gov.uk/",
+  }
+
+  rabbitmq_mgmt_client = RabbitMQ::HTTP::Client.new("http://guest:guest@127.0.0.1:15672")
+
+  subject { GovukSeedCrawler::Seeder::seed(options) }
+  let(:probable_min_number_of_urls) { 1000 }
+
+  before(:each) do
+    rabbitmq_mgmt_client.declare_exchange(options[:amqp_vhost], options[:amqp_exchange], { :type => options[:amqp_exchange_type] })
+  end
+
+  after(:each) do
+    rabbitmq_mgmt_client.delete_exchange(options[:amqp_vhost], options[:amqp_exchange])
+  end
+
+  it "publishes URLs it finds to an AMQP topic exchange" do
+    subject
+
+    sleep(1) # give the management API time to recognise the messages we've just published
+    exchange_info = rabbitmq_mgmt_client.exchange_info("/", options[:amqp_exchange])
+    number_of_messages_on_exchange = exchange_info["message_stats"]["publish_in"]
+    expect(number_of_messages_on_exchange).to be > probable_min_number_of_urls
+  end
+end
