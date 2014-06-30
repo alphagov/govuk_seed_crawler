9eb51bd88325cc2a10175fecb3ee983da6a1585d
Matt Bostock <matt@mattbostock.com>
2014-06-30 17:26:29 +0100

    Connect to RabbitMQ first
    So that we fail fast if the RabbitMQ connection fails, for example if no
    password has been specified on the CLI.
    
    I was worried that by doing this, we risk the RabbitMQ timing out while
    we retrieve URLs from the content API but Bunny, the library we're using
    to connect to RabbitMQ, uses a heartbeat to keep the connection alive:
    https://github.com/ruby-amqp/bunny/blob/72376c8488effc434d16accd6d5ff008b24b1720/lib/bunny/session.rb#L999

--- lib/govuk_seed_crawler/seeder.rb
+++ lib/govuk_seed_crawler/seeder.rb
@@ -1,11 +1,12 @@
 module GovukSeedCrawler
   class Seeder
     def self.seed(options = {})
-      urls = GetUrls.new(options[:site_root]).urls
       topic_connection = TopicExchange.new(options)
       topic_exchange = topic_connection.exchange
 
       GovukSeedCrawler.logger.info("Retrieving list of URLs for #{options[:site_root]}")
+      urls = GetUrls.new(options[:site_root]).urls
+
       PublishUrls::publish(topic_exchange, options[:amqp_topic], urls)
 
       topic_connection.close
