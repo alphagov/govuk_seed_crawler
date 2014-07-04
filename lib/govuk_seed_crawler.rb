cfbd8028f9b79fa389258e52fb72294d2fe93682
Matt Bostock <matt@mattbostock.com>
2014-07-04 15:27:25 +0100

    Rename `get_urls.rb` to `indexer.rb`
    To reflect the renaming of that class in the previous commit.

--- lib/govuk_seed_crawler.rb
+++ lib/govuk_seed_crawler.rb
@@ -1,6 +1,6 @@
 require 'govuk_seed_crawler/amqp_client'
 require 'govuk_seed_crawler/cli'
-require 'govuk_seed_crawler/get_urls'
+require 'govuk_seed_crawler/indexer'
 require 'govuk_seed_crawler/seeder'
 require 'govuk_seed_crawler/url_publisher'
 require 'govuk_seed_crawler/version'
