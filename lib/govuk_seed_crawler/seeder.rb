fc3ac9cc37c3d1d0249ed3f2d4bd65ee95db0423
Matt Bostock <matt@mattbostock.com>
2014-07-04 11:23:34 +0100

    Logic error when extracting AMQP connection opts
    Members of the `options` array are rejected if the block passed to
    `reject` returns true; the logic we desire is the inverse of what I had
    previously.

--- lib/govuk_seed_crawler/seeder.rb
+++ lib/govuk_seed_crawler/seeder.rb
@@ -2,7 +2,7 @@ module GovukSeedCrawler
   class Seeder
     def self.seed(options = {})
       amqp_connection_options = options.reject do |key, _value|
-        %w{:amqp_host :amqp_port :amqp_username :amqp_password}.include?(key)
+        %w{:amqp_host :amqp_port :amqp_username :amqp_password}.include?(key) == false
       end
 
       topic_connection = TopicExchange.new(options[:amqp_exchange], amqp_connection_options)
