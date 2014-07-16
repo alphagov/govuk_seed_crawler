083fb02147245dcd618707b954dba8150c4858b8
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-07-16 10:43:29 +0100

    [#75033132] --exchange and --topic correctly passed through to Seeder
    The fixes a bug where we were using different keys to refer to the
    same value. As a result, we weren't passing through values to the
    objects we were using on the command line.

--- lib/govuk_seed_crawler/seeder.rb
+++ lib/govuk_seed_crawler/seeder.rb
@@ -6,10 +6,10 @@ module GovukSeedCrawler
       amqp_client = AmqpClient.new(options)
 
       urls.each do |url|
-        amqp_client.publish(options[:amqp_exchange], options[:amqp_topic], url)
+        amqp_client.publish(options[:exchange], options[:topic], url)
       end
 
-      GovukSeedCrawler.logger.info("Published #{urls.count} URLs to topic '#{options[:amqp_topic]}'")
+      GovukSeedCrawler.logger.info("Published #{urls.count} URLs to topic '#{options[:topic]}'")
 
       amqp_client.close
     end
