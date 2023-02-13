dd88b5580bcbd5ec8a83913ea07190f391451514
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2023-02-13 14:38:55 +0000

    Update usage of 'Slop' from v3 to v4
    This commit does the following:
    
    - Rename `parse` to `parse!`
      The `parse!` method was removed in 2014
      (https://github.com/leejarvis/slop/commit/9c83e0aec21d669a99d6c97cefb8eac793ffa73c#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5R317)
      not long after slop was added to govuk_seed_crawler
      (https://github.com/alphagov/govuk_seed_crawler/commit/9980080b47388c93d27be9a69086910474a81e4f).
    - Use `Slop::Options` object and define type-aware flags.
      That's [how things are done in slop now](https://github.com/leejarvis/slop#usage).
    - Revamp the '-h' help flag.
      The return value of `Slop.parse` is now a `Slop::Result` which
      "provides a nice help string to display your options. Just
      `puts opts` or call `opts.to_s`":
      https://github.com/leejarvis/slop#printing-help
      I've also removed `help: true` as this no longer does anything,
      and was giving an `ArgumentError: wrong number of arguments (given 2, expected 1)`
    - Improved the logic around validating that the correct number of
      args are passed.
      We use the `opts.arguments` method to surface any arguments that
      were not parsed, i.e. the non-flag arguments.
      We remove the `if opts[:version].nil?`, which was just a confusing
      hack (i.e. you should be able to provide `--version` without
      providing a site root, as you're only wanting to see the version
      number of govuk_seed_crawler at that point, so no validation on
      the args is necessary there).
    - Removes `nil` values from the defaults, as these are now ignored
      from `to_hash` anyway:
      https://github.com/leejarvis/slop#option-types

--- lib/govuk_seed_crawler/cli_parser.rb
+++ lib/govuk_seed_crawler/cli_parser.rb
@@ -13,7 +13,6 @@ module GovukSeedCrawler
   class CLIParser
     DEFAULTS = {
       exchange: "govuk_crawler_exchange",
-      help: nil,
       host: "localhost",
       password: "guest",
       port: "5672",
@@ -21,7 +20,6 @@ module GovukSeedCrawler
       topic: "#",
       username: "guest",
       verbose: false,
-      version: nil,
       vhost: "/",
     }.freeze
 
@@ -32,45 +30,48 @@ module GovukSeedCrawler
     end
 
     def options
-      Slop.parse!(@argv_array, help: true) do
-        banner <<~HELP
-          Usage: #{$PROGRAM_NAME} site_root [options]
+      opts = Slop::Options.new
+      opts.banner = <<~HELP
+        Usage: #{$PROGRAM_NAME} site_root [options]
 
-          Seeds an AMQP topic exchange with messages, each containing a URL, for the GOV.UK Crawler Worker
-          to consume:
+        Seeds an AMQP topic exchange with messages, each containing a URL, for the GOV.UK Crawler Worker
+        to consume:
 
-          https://github.com/alphagov/govuk_crawler_worker
+        https://github.com/alphagov/govuk_crawler_worker
 
-          The AMQP password can also be set as an environment variable and will be read from
-          `#{ENV_AMQP_PASS_KEY}`. If both the environment variable and command-line option for password
-          are set, the environment variable will take higher precedent.
-        HELP
-
-        on :version, "Display version and exit" do
-          puts "Version: #{GovukSeedCrawler::VERSION}"
-          exit 0
-        end
-
-        on :host=, "AMQP host to publish to", default: DEFAULTS[:host]
-        on :port=, "AMQP port", default: DEFAULTS[:port]
-        on :username=, "AMQP username", default: DEFAULTS[:username]
-        on :password=, "AMQP password", default: DEFAULTS[:password]
-        on :exchange=, "AMQP exchange", default: DEFAULTS[:exchange]
-        on :topic=, "AMQP topic", default: DEFAULTS[:topic]
-        on :vhost=, "AMQP vhost", default: DEFAULTS[:vhost]
-
-        on :quiet, "Quiet output", default: DEFAULTS[:quiet]
-        on :verbose, "Verbose output", default: DEFAULTS[:verbose]
+        The AMQP password can also be set as an environment variable and will be read from
+        `#{ENV_AMQP_PASS_KEY}`. If both the environment variable and command-line option for password
+        are set, the environment variable will take higher precedent.
+      HELP
+      opts.string "--host", "AMQP host to publish to", default: DEFAULTS[:host]
+      opts.string "--port", "AMQP port", default: DEFAULTS[:port]
+      opts.string "--username", "AMQP username", default: DEFAULTS[:username]
+      opts.string "--password", "AMQP password", default: DEFAULTS[:password]
+      opts.string "--exchange", "AMQP exchange", default: DEFAULTS[:exchange]
+      opts.string "--topic", "AMQP topic", default: DEFAULTS[:topic]
+      opts.string "--vhost", "AMQP vhost", default: DEFAULTS[:vhost]
+      opts.bool "-q", "--quiet", "Quiet output", default: DEFAULTS[:quiet]
+      opts.bool "-v", "--verbose", "Verbose output", default: DEFAULTS[:verbose]
+      opts.on "--version", "Display version and exit" do
+        puts "Version: #{GovukSeedCrawler::VERSION}"
+        exit 0
       end
+      opts.on "-h", "--help" do
+        puts opts
+        exit
+      end
+      parser = Slop::Parser.new(opts)
+      parser.parse(@argv_array)
     end
 
     def parse
       opts = options
 
-      if opts[:version].nil?
-        raise CLIException.new("too many arguments provided", opts.help) if @argv_array.size > 1
-        raise CLIException.new("site_root must be provided", opts.help) if @argv_array.size != 1
-      end
+      # opts.arguments shows all arguments NOT processed by the parser,
+      # which should just be the first arg (site root).
+      # See https://github.com/leejarvis/slop#arguments
+      raise CLIException.new("too many arguments provided", opts.to_s) if opts.arguments.count > 1
+      raise CLIException.new("site_root must be provided", opts.to_s) if opts.arguments.count.zero?
 
       options_hash = opts.to_hash
       options_hash[:password] = ENV[ENV_AMQP_PASS_KEY] unless ENV[ENV_AMQP_PASS_KEY].nil?
